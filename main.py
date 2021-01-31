2019fd671a113efdd2fc0562db46da6f78f91163
Chih-Chung Kuo <kuoc@seagroup.com>
2021-01-31 23:09:45 +0800

    Fix text wrongly parsed issue

--- main.py
+++ main.py
@@ -8,45 +8,47 @@ from timezone import get_timezone
 DATE_BIN = '/usr/local/opt/coreutils/libexec/gnubin/date'
 
 FORMATS = [
-	"+%s",
-	# 1937-01-01 12:00:27
-	"+%Y-%m-%d %H:%M:%S",
-	# 19 May 2002 15:21:36
-	"+%d %b %Y %H:%M:%S",
-	# Sun, 19 May 2002 15:21:36
-	"+%a, %d %b %Y %H:%M:%S",
-	# 1937-01-01T12:00:27
-	"+%Y-%m-%dT%H:%M:%S",
-	# 19370101120027
-	"+%Y%m%d%H%M%S",
-	# 1996-12-19T16:39:57-0800
-	"+%Y-%m-%dT%H:%M:%S%z",
-	"+%Y-%m-%d %H:%M:%S %Z",
+		"+'%s'",
+		# 1937-01-01 12:00:27
+		"+'%Y-%m-%d %H:%M:%S'",
+		# 19 May 2002 15:21:36
+		"+'%d %b %Y %H:%M:%S'",
+		# Sun, 19 May 2002 15:21:36
+		"+'%a, %d %b %Y %H:%M:%S'",
+		# 1937-01-01T12:00:27
+		"+'%Y-%m-%dT%H:%M:%S'",
+		# 19370101120027
+		"+'%Y%m%d%H%M%S'",
+		# 1996-12-19T16:39:57-0800
+		"+'%Y-%m-%dT%H:%M:%S%z'",
+		"+'%Y-%m-%d %H:%M:%S %Z'",
 ]
 
 def exec_cmd(timezone, from_format="now", to_format=""):
-	command = "TZ={tz} {command} {to_format} --date={from_format}".format(
-		tz=timezone,
-		command=DATE_BIN,
-		to_format=to_format,
-		from_format=from_format,
+	command = "TZ={tz} {command} {to_format} --date=\"{from_format}\"".format(
+			tz=timezone,
+			command=DATE_BIN,
+			to_format=to_format,
+			from_format=from_format,
 	)
 	return [i.strip() for i in subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()]
 
 def get_datetime(wf, dt_format, input_str):
 	timezone, tzoffset = get_timezone(wf)
-	input_str = input_str and input_str.strip() or 'now'
 
+	# Unify input_str
+	input_str = input_str and input_str.strip() or 'now'
+	input_str = input_str.replace('tmr', 'tomorrow').replace('ytd', 'yesterday')
 	if input_str.isdigit():
-	    input_str = "@{}".format(input_str)
+		input_str = "@{}".format(input_str)
 
 	rc = re.findall(r'^(@\d+)(\s.*){0,1}', input_str)
 	if rc:
 		timestamp, successor = rc[0]
-		rc, _ = exec_cmd(timezone, '"{}"'.format(timestamp))
+		rc, _ = exec_cmd(timezone, '{}'.format(timestamp))
 		input_str = "{} {}".format(rc, successor)
 
-	return exec_cmd(timezone, repr(input_str), repr(dt_format))
+	return exec_cmd(timezone, input_str, dt_format)
 
 
 def check_datetime_bin():
